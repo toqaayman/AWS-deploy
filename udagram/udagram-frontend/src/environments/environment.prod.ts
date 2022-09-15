985bc6f843c72d1b1509ef25aaffb48115a6d61e
Toqa Ayman <53654509+toqaayman@users.noreply.github.com>
2022-09-15 23:03:22 +0200

    first commit

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://localhost:8080/api/v0",
+  apiHost: "http://udagram-api2.eba-b4yayrtx.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
