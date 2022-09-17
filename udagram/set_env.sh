0ea106b3cc335266113169a942e3146e54abb202
Toqa Ayman <53654509+toqaayman@users.noreply.github.com>
2022-09-17 17:11:11 +0200

    working..

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -1,10 +1,10 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=131119
-export POSTGRES_HOST=database-1.cawh6kunp8i1.us-east-1.rds.amazonaws.com
+export POSTGRES_PASSWORD=Tokatoka12
+export POSTGRES_HOST=database-2.cawh6kunp8i1.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::myawsbucket-75139724085
+export AWS_BUCKET=arn:aws:s3:::toqaayman-udagram
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=mysecretstring
